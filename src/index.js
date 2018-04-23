43b9f28d9f19b468c8ab102ca04cf0ff45ca5463
vsparrow <khan@vsparrow.com>
2018-04-23 14:34:48 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 // Code Goes Here
+// PROPS
+// cone — a boolean indicating if the ice cream should be in a cone, defaults to true
+// size — a string to indicate the size of the order, defaults to 'regular'
+// scoops — an array of ice cream flavors
+// orderInfo — an object containing data about the ice cream order
+class Order extends React.Component{
+  render(){
+    return(
+      <div className="order">
+        <ul>
+          <li>{this.props.cone ? 'Cone' : ' Cup'}</li>
+          <li>{this.props.size}</li>
+          <li>{this.props.scoops.length} scoops:
+            {this.props.scoops.join(", ")}
+          </li>
+          <li>Ordered by {this.props.orderInfo.customerName} at
+            {this.props.orderInfo.orderedAt}
+          </li>
+        </ul>
+    </div>
+    )
+  }
+}
+
+Order.defaultProps = {
+  cone:true,
+  size: 'regular'
+}
+
+Order.propTypes = {
+  cone: PropTypes.bool,
+  size: PropTypes.string,
+  scoops: PropTypes.array(PropTypes.string).isRequired, // tell it we're expecting an array of strings:
+  //React.PropTypes.shape, we can tell our component to expect the prop to have a certain shape:
+  // orderInfo: PropTypes.object.isRequired
+  orderInfo: PropTypes.object.shape({
+    customerName: PropTypes.string.isRequired,
+    orderedAt: PropTypes.number.isRequired
+  }).isRequired
+}
